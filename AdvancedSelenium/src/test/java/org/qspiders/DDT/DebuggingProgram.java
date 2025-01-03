841deae51e6b921b3653abfdc48e3ff6a49aea6e
Pavan <Pavan@LAPTOP-FATP5U09>
2025-01-03 07:41:45 +0530

    push3

--- AdvancedSelenium/src/test/java/org/qspiders/DDT/DebuggingProgram.java
+++ AdvancedSelenium/src/test/java/org/qspiders/DDT/DebuggingProgram.java
@@ -5,10 +5,13 @@ public class DebuggingProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Program to debug push after modification
+		//push again after modification
 		int a = 10;
 		int b = 20;
+		int c = 30;
 		System.out.println(a);
 		System.out.println(b);
+		System.out.println(c);
 		System.out.println("=============================================================");
 		int temp;
 		temp=a;
